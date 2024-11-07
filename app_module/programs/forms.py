4f21852d72981cfdd3d10407ed0b87bce1ad7786
Caine Bautista <115358604+onLine69@users.noreply.github.com>
2024-11-07 10:15:52 +0800

    Added cancel button to forms

--- app_module/programs/forms.py
+++ app_module/programs/forms.py
@@ -5,4 +5,4 @@ from wtforms import StringField, validators, SelectField
 class ProgramForm(FlaskForm):
     program_code = StringField(label="Program Code", validators=[validators.DataRequired(message="Provide a code."), validators.length(min=1,max=10,message="Length not valid, should be between 1 and 10 inclusive.")])
     program_name = StringField(label="Program Name", validators=[validators.DataRequired(message="Provide a name."), validators.length(min=1,max=100,message="Length not valid. should be between 1 and 100 inclusive.")])
-    college_code = SelectField(label="Program", choices=[])
+    college_code = SelectField(label="College", choices=[])
